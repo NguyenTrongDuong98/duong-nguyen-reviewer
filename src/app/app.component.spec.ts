0c77d04dc514392f19d40e327cdddac8071e64cd
nguyentrongduong <nguyentrongduong98@gmail.com>
2023-03-09 10:39:37 +0700

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'duong-nguyen-reviewer'`, () => {
+  it(`should have as title 'my-bio-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('duong-nguyen-reviewer');
+    expect(app.title).toEqual('my-bio-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('duong-nguyen-reviewer app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('my-bio-app app is running!');
   });
 });
